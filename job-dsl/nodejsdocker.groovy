2168cb11cfc01cc89cc43c7cd422a267b28dd056
gramae <grama_emi@yahoo.com>
2021-02-09 12:24:12 +0200

    updating credentials

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git@github.com:gramae/jenkins-nodejs.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('gramae')
+            node / gitConfigEmail('emanuel.grama@oracle.com')
         }
     }
     triggers {
